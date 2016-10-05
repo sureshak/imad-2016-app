9d7e541768072e8cee23c7e185f36aa0742a96ea
sureshak <sureshak07@gmail.com>
2016-10-05 21:09:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
-var nameInput = document.getElementById("name1"); 
-console.log(nameInput);
-var submit= document.getElementById("submitbtn");
-submit.onclick = function(){
-    alert();
-    
-}
+var nameInput = document.getElementById("name").value;
+            var submit= document.getElementById("submitbtn");
+            submit.onclick = function(){
+             var names = ['name1','name2','name3'];
+             var list='';
+             for(var i=0;i<names.length;i++){
+                 console.log(names[i]);
+                 list += '<li>'+names[i]+'</li>';
+                 
+             }
+             var ul=document.getElementById("testId");
+             ul.innerHTML=list;
+            }
