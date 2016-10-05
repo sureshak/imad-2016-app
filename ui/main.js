ef07b299c297ea77fff79228225912205878438e
sureshak <sureshak07@gmail.com>
2016-10-05 14:32:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var nameInput = document.getElementById("name1"); 
 console.log(nameInput);
 var submit= document.getElementById("submitbtn");
-submit.onClick = function(){
+submit.onclick = function(){
     alert();
     
 }
